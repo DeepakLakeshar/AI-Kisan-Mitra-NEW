9adfb18c22de7d839a4144ef8d07034a90e171af
DeepakLakeshar <197962230+DeepakLakeshar@users.noreply.github.com>
2026-05-03 22:30:52 +0530

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  turbopack: {
+    root: __dirname,
+  },
+};
+
+export default nextConfig;
